1b1fcb2099b3a4b232b04fb7738669228ffe75d0
NightSparrows <109325365+NightSparrows@users.noreply.github.com>
2023-03-08 22:28:36 +0800

    Update addtaskui.py
    test for adding datetime

--- game/fgo/ui/addtaskui.py
+++ game/fgo/ui/addtaskui.py
@@ -1,4 +1,5 @@
 
+import time
 import datetime
 
 from core.util.stringutil import StringUtil
@@ -120,7 +121,7 @@ class AddTaskUI:
                     print('Key輸入錯誤')
                     return False
 
-                task = QPTask(game, battle, count, datetime.datetime.now().date(), True)
+                task = QPTask(game, battle, count, datetime.datetime.combine(datetime.datetime.now().date(), time(0)), True)
                 game._taskManager.addTask(task)
                 print('QPTask新增成功')
                 return True
